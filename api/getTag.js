dff9b94b08706b2f2bdd4964d9a9d6007e7d2561
Zane St. John <zstjohn@stanford.edu>
2023-03-08 12:49:14 -0800

    more api work

--- api/getTag.js
+++ api/getTag.js
@@ -1,10 +1,14 @@
 const fetch = require("node-fetch");
+const { parseString } = require("xml2js");
 
 export default async function getTag(req, res) {
   const apiRequest = await fetch(
     "https://www.barbershoptags.com/api.php?id=" +
       encodeURIComponent(req.query.id)
   );
-  const apiResponse = await apiRequest.text();
-  res.status(200).send({ text: apiResponse });
+  parseString(await apiRequest.text(), function parse(err, result) {
+    err
+      ? res.status(500).send({ error: err })
+      : res.status(200).send({ response: result });
+  });
 }
