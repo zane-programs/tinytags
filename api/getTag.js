4c458f2090471be324c29f6e5a65c59048eae500
Zane St. John <zstjohn@stanford.edu>
2023-03-08 12:53:01 -0800

    api should work now

--- api/getTag.js
+++ api/getTag.js
@@ -7,8 +7,18 @@ export default async function getTag(req, res) {
       encodeURIComponent(req.query.id)
   );
   parseString(await apiRequest.text(), function parse(err, result) {
+    const tag = result.tags.tag[0];
+
     err
       ? res.status(500).send({ error: err })
-      : res.status(200).send({ response: result });
+      : res
+          .status(200)
+          .send({
+            name: tag.Title[0],
+            sheetMusic: {
+              url: tag.SheetMusicAlt[0],
+              fileType: tag.SheetMusic[0].$.type,
+            },
+          });
   });
 }
