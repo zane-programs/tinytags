6b061852fd5ebe657755492d467d7297beec8008
Zane St. John <zstjohn@stanford.edu>
2023-03-08 16:20:24 -0800

    tidy up getTags

--- api/getTag.js
+++ api/getTag.js
@@ -1,47 +1,31 @@
 const fetch = require("node-fetch");
 const { parseString } = require("xml2js");
 
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
-
-async function getTag(req, res) {
+export default async function getTag(req, res) {
   const apiRequest = await fetch(
     "https://www.barbershoptags.com/api.php?id=" +
       encodeURIComponent(req.query.id)
   );
   parseString(await apiRequest.text(), function parse(err, result) {
-    const tag = result.tags.tag[0];
+    // Parse error => 500
+    if (err) {
+      return res.status(500).send({ error: err });
+    }
 
-    err
-      ? res.status(500).send({ error: err })
-      : res
-          .status(200)
-          .send({
-            id: tag.id[0],
-            name: tag.Title[0],
-            sheetMusic: {
-              url: tag.SheetMusicAlt[0],
-              fileType: tag.SheetMusic[0].$.type,
-            },
-          });
+    // No tags available => 404
+    if (parseInt(result.tags.$.available) === 0) {
+      return res.status(404).send({ error: "Tag not found" });
+    }
+
+    // Otherwise => Parse tag and 200 w/ results
+    const tag = result.tags.tag[0];
+    res.status(200).send({
+      id: tag.id[0],
+      name: tag.Title[0],
+      sheetMusic: {
+        url: tag.SheetMusicAlt[0],
+        fileType: tag.SheetMusic[0].$.type,
+      },
+    });
   });
 }
-
-export default allowCors(getTag);
