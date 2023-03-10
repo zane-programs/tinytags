7b85569a8a13742b87c0e44b61182b57db07ba5e
Zane St. John <zstjohn@stanford.edu>
2023-03-09 16:37:24 -0800

    1.1.0 - added 'fave' functionality

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
 (function (window, document) {
+  const EMPTY_FAVE = { index: -1, value: "-1" };
+
   let $tagNumber;
   let currentTagNumber = "";
+  let currentFave = EMPTY_FAVE;
+  let isViewingFave = false;
 
   function setCurrentTagNumber(num) {
     currentTagNumber = num;
-    $tagNumber.innerHTML = currentTagNumber;
+    $tagNumber.innerHTML =
+      (isViewingFave ? "#" + (currentFave.index + 1) + ": " : "") +
+      currentTagNumber;
   }
 
   function viewTag(num) {
@@ -13,29 +19,72 @@
     }
   }
 
+  function resetCurrentFave() {
+    isViewingFave = false;
+    currentFave = EMPTY_FAVE;
+    $tagNumber.classList.remove("fave");
+  }
+  function setCurrentFave(index, value) {
+    isViewingFave = true;
+    currentFave = { index: index, value: value };
+    setCurrentTagNumber(currentFave.value);
+    // add fave styling depending
+    $tagNumber.classList.add("fave");
+  }
+
+  function getFaves() {
+    return JSON.parse(localStorage.getItem("faves"));
+  }
+  function nextFave(faves) {
+    const nextIndex = (currentFave.index + 1) % faves.length;
+    console.log(nextIndex, faves[nextIndex]);
+    setCurrentFave(nextIndex, faves[nextIndex]);
+  }
+  function prevFave(faves) {
+    const prevIndex =
+      currentFave.index === 0 ? faves.length - 1 : currentFave.index - 1;
+    console.log(prevIndex, faves[prevIndex]);
+    setCurrentFave(prevIndex, faves[prevIndex]);
+  }
+
+  function handleFaveNavigation(key) {
+    let faves = getFaves();
+    if (faves.length === 0) return;
+    // Navigate
+    key === "ArrowUp" ? prevFave(faves) : nextFave(faves);
+  }
+
   window.addEventListener("keydown", ({ key }) => {
     if (/\b[0-9]\b/.test(key)) {
       setCurrentTagNumber(currentTagNumber + key);
     } else {
       switch (key) {
+        case "ArrowUp":
+        case "ArrowDown":
+          handleFaveNavigation(key);
+          break;
         case "#":
         case "Backspace":
+          resetCurrentFave();
           setCurrentTagNumber(
             currentTagNumber.substring(0, currentTagNumber.length - 1)
           );
           break;
+        case "*":
+          resetCurrentFave();
+          setCurrentTagNumber("");
+          break;
         case "Enter":
           viewTag(currentTagNumber);
           break;
-        case "*":
-          setCurrentTagNumber("");
-          break;
       }
     }
-    console.log(key);
   });
 
   document.addEventListener("DOMContentLoaded", () => {
+    if (!localStorage.getItem("faves")) {
+      localStorage.setItem("faves", "[]");
+    }
     $tagNumber = document.getElementById("tagNumber");
   });
 })(window, document);
--- js/tag.js
+++ js/tag.js
@@ -3,8 +3,12 @@
   const ZOOM_INCREMENT_PERCENT = 0.1;
   const INITIAL_ARROW_KEY_DELTA = 14;
 
-  // selector for #tagView
+  // selectors
   let $tagView;
+  let $messageModal;
+
+  // container for api response
+  let tagData;
 
   // - variables zoom and arrow-key
   let zoomFactor = 1;
@@ -13,6 +17,30 @@
   // variables - x and y position
   let x = 0;
   let y = 0;
+
+  let messageModalHideTimeout = -1;
+  function showModalMessage(
+    message,
+    backgroundColor = "#f00",
+    textColor = "#000",
+    duration = 1000
+  ) {
+    // clear hiding timeout
+    clearTimeout(messageModalHideTimeout);
+
+    // stage modal
+    $messageModal.style.backgroundColor = backgroundColor;
+    $messageModal.style.color = textColor;
+    $messageModal.innerHTML = message;
+
+    // show modal
+    $messageModal.style.display = null;
+
+    // set hiding timeout with given duration
+    messageModalHideTimeout = setTimeout(function () {
+      $messageModal.style.display = "none";
+    }, duration);
+  }
 
   // update #tagView transform style (scale and translate)
   function updateTransform() {
@@ -34,7 +62,52 @@
     updateTransform();
   }
 
-  window.addEventListener("keydown", ({ key }) => {
+  //showModalMessage("ur mom", "#00f", "#fff", 5000);
+
+  function getFaves() {
+    return JSON.parse(localStorage.getItem("faves"));
+  }
+  function setFaves(faves) {
+    localStorage.setItem("faves", JSON.stringify(faves));
+  }
+
+  function isInFaves() {
+    let faves = getFaves();
+    return (
+      faves.find(function (fave) {
+        return fave[0] === tagData.id;
+      }) !== undefined
+    );
+  }
+  function addToFaves() {
+    let faves = getFaves();
+    // add it
+    faves.push([tagData.id, tagData.name]);
+    // set it
+    setFaves(faves);
+  }
+  function removeFromFaves() {
+    let faves = getFaves();
+    // filter out this id
+    faves = faves.filter(function (fave) {
+      return fave[0] !== tagData.id;
+    });
+    // set it
+    setFaves(faves);
+  }
+
+  function toggleFave() {
+    // don't allow until loaded
+    if (!tagData) return;
+
+    let inFaves = isInFaves();
+    inFaves ? removeFromFaves() : addToFaves();
+    showModalMessage(inFaves ? "unfaved" : "faved", "#ff4be8", "#fff", 1500);
+  }
+
+  window.addEventListener("keydown", function (e) {
+    const key = e.key;
+
     switch (key) {
       case "ArrowUp":
         // up
@@ -68,15 +141,23 @@
         setX(0);
         setY(0);
         break;
+
+      case "2":
+        // toggle favorite on tag
+        toggleFave();
     }
   });
 
   document.addEventListener("DOMContentLoaded", async function () {
     $tagView = document.getElementById("tagView");
+    $messageModal = document.getElementById("messageModal");
 
     // api request
     const req = await fetch("/api/getTag" + window.location.search);
     const res = await req.json();
+
+    // add response data to container variable
+    tagData = res;
 
     // haha error handling
     if (res.error) {
@@ -88,7 +169,7 @@
     if (res.sheetMusic.fileType.toLowerCase() === "pdf") {
       // pdf - embed as google drive pdf embed
       sheetMusicElement = document.createElement("embed");
-      embed.src =
+      sheetMusicElement.src =
         "https://drive.google.com/viewerng/viewer?embedded=true&chrome=false&url=" +
         encodeURIComponent(res.sheetMusic.url);
     } else {
