3d79b0f48f1dd3e4eba665bdfb6a138f6f3e05b1
Zane St. John <zstjohn@stanford.edu>
2023-03-15 15:03:26 -0700

    1.2.3 - added pdf stuff to v2/getTag.js

--- api/v2/getTag.js
+++ api/v2/getTag.js
@@ -1,6 +1,25 @@
 const fetch = require("node-fetch")
 const { parseString } = require("xml2js");
 const convertPDFToImageURLs = require("../../pdf");
+const { pdf } = require("pdf-to-img");
+
+async function convertPDFToImageURLs(url) {
+  const response = await fetch(url, {
+    responseType: "arraybuffer",
+  });
+
+  const pdfPages = await pdf(Buffer.from(await response.buffer(), "binary"), {
+    scale: 2,
+  });
+
+  let imageURLs = [];
+  for await (const page of pdfPages) {
+    imageURLs.push("data:img/png;base64," + page.toString("base64"));
+  }
+
+  return imageURLs;
+};
+
 
 function handleError(res, e) {
   res.status(500).send({ error: e?.message || e });
