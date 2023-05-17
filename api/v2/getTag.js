9e1d8731963cc8120c5a93a474d786eca4899663
Zane St. John <zstjohn@stanford.edu>
2023-05-16 20:32:19 -0700

    comment out v2 lol

--- api/v2/getTag.js
+++ api/v2/getTag.js
@@ -1,75 +1,75 @@
-const fetch = require("node-fetch")
-const { parseString } = require("xml2js");
-const { pdf } = require("pdf-to-img");
+// const fetch = require("node-fetch")
+// const { parseString } = require("xml2js");
+// const { pdf } = require("pdf-to-img");
 
-async function convertPDF(url) {
-  const response = await fetch(url, {
-    responseType: "arraybuffer",
-  });
+// async function convertPDF(url) {
+//   const response = await fetch(url, {
+//     responseType: "arraybuffer",
+//   });
 
-  const pdfPages = await pdf(Buffer.from(await response.buffer(), "binary"), {
-    scale: 2,
-  });
+//   const pdfPages = await pdf(Buffer.from(await response.buffer(), "binary"), {
+//     scale: 2,
+//   });
 
-  let imageURLs = [];
-  for await (const page of pdfPages) {
-    imageURLs.push("data:img/png;base64," + page.toString("base64"));
-  }
+//   let imageURLs = [];
+//   for await (const page of pdfPages) {
+//     imageURLs.push("data:img/png;base64," + page.toString("base64"));
+//   }
 
-  return imageURLs;
-};
+//   return imageURLs;
+// };
 
 
-function handleError(res, e) {
-  res.status(500).send({ error: e?.message || e });
-}
+// function handleError(res, e) {
+//   res.status(500).send({ error: e?.message || e });
+// }
 
-export default async function getTag(req, res) {
-  try {
-    const apiRequest = await fetch(
-      "https://www.barbershoptags.com/api.php?id=" +
-        encodeURIComponent(req.query.id)
-    );
-    parseString(await apiRequest.text(), async function parse(err, result) {
-      // Parse error => 500
-      if (err) {
-        return res.status(500).send({ error: err });
-      }
+// export default async function getTag(req, res) {
+//   try {
+//     const apiRequest = await fetch(
+//       "https://www.barbershoptags.com/api.php?id=" +
+//         encodeURIComponent(req.query.id)
+//     );
+//     parseString(await apiRequest.text(), async function parse(err, result) {
+//       // Parse error => 500
+//       if (err) {
+//         return res.status(500).send({ error: err });
+//       }
 
-      try {
-        // Server error on BarbershopTags.com API side
-        if (!("tags" in result) && result?.pre) {
-          return res.status(500).send({
-            error:
-              "BarbershopTags.com API: " +
-              (result.pre?._.trim() || "Unknown error"),
-          });
-        }
+//       try {
+//         // Server error on BarbershopTags.com API side
+//         if (!("tags" in result) && result?.pre) {
+//           return res.status(500).send({
+//             error:
+//               "BarbershopTags.com API: " +
+//               (result.pre?._.trim() || "Unknown error"),
+//           });
+//         }
 
-        // No tags available => 404
-        if (parseInt(result.tags.$.available) === 0) {
-          return res.status(404).send({ error: "Tag not found" });
-        }
+//         // No tags available => 404
+//         if (parseInt(result.tags.$.available) === 0) {
+//           return res.status(404).send({ error: "Tag not found" });
+//         }
 
-        // Otherwise => Parse tag and 200 w/ results
-        const tag = result.tags.tag[0];
-        const fileType = tag.SheetMusic[0].$.type.toLowerCase();
+//         // Otherwise => Parse tag and 200 w/ results
+//         const tag = result.tags.tag[0];
+//         const fileType = tag.SheetMusic[0].$.type.toLowerCase();
 
-        const sheetMusicImageArray =
-          fileType === "pdf"
-            ? await convertPDF(tag.SheetMusicAlt[0])
-            : [tag.SheetMusicAlt[0]];
+//         const sheetMusicImageArray =
+//           fileType === "pdf"
+//             ? await convertPDF(tag.SheetMusicAlt[0])
+//             : [tag.SheetMusicAlt[0]];
 
-        res.status(200).send({
-          id: tag.id[0],
-          name: tag.Title[0],
-          sheetMusic: sheetMusicImageArray,
-        });
-      } catch (e) {
-        handleError(res, e);
-      }
-    });
-  } catch (e) {
-    handleError(res, e);
-  }
-}
+//         res.status(200).send({
+//           id: tag.id[0],
+//           name: tag.Title[0],
+//           sheetMusic: sheetMusicImageArray,
+//         });
+//       } catch (e) {
+//         handleError(res, e);
+//       }
+//     });
+//   } catch (e) {
+//     handleError(res, e);
+//   }
+// }
