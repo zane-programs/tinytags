d64fb3f7033939d9b1d3e0cff19ff2021e63e70b
Zane St. John <zstjohn@stanford.edu>
2023-05-16 20:31:12 -0700

    add back legacy api

---
+++ api/getTag.js
@@ -0,0 +1,52 @@
+const fetch = require("node-fetch");
+const { parseString } = require("xml2js");
+
+function handleError(res, e) {
+  res.status(500).send({ error: e?.message || e });
+}
+
+export default async function getTag(req, res) {
+  try {
+    const apiRequest = await fetch(
+      "https://www.barbershoptags.com/api.php?id=" +
+        encodeURIComponent(req.query.id)
+    );
+    parseString(await apiRequest.text(), function parse(err, result) {
+      // Parse error => 500
+      if (err) {
+        return res.status(500).send({ error: err });
+      }
+
+      try {
+        // Server error on BarbershopTags.com API side
+        if (!("tags" in result) && result?.pre) {
+          return res.status(500).send({
+            error:
+              "BarbershopTags.com API: " +
+              (result.pre?._.trim() || "Unknown error"),
+          });
+        }
+
+        // No tags available => 404
+        if (parseInt(result.tags.$.available) === 0) {
+          return res.status(404).send({ error: "Tag not found" });
+        }
+
+        // Otherwise => Parse tag and 200 w/ results
+        const tag = result.tags.tag[0];
+        res.status(200).send({
+          id: tag.id[0],
+          name: tag.Title[0],
+          sheetMusic: {
+            url: tag.SheetMusicAlt[0],
+            fileType: tag.SheetMusic[0].$.type,
+          },
+        });
+      } catch (e) {
+        handleError(res, e);
+      }
+    });
+  } catch (e) {
+    handleError(res, e);
+  }
+}
